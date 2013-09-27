7d4466d370170e195791dc2d95c3fbf69d979aa9
andry360 <andry360@hotmail.it>
2013-09-27 18:18:32 +0200

    added mysql class

---
+++ mysql.php
@@ -0,0 +1,12 @@
+<?php
+
+class mysql {
+
+	public function array_loop() {
+		$con=mysqli_connect("192.168.30.51","andreatest","andreatest","andreatest");
+	    if (mysqli_connect_errno($con)){
+			echo "failed" . mysqli_connnect_error(); }
+	}
+}
+
+?>
